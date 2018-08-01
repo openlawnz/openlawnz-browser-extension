26857a81ba8b40eab92ab677847c78785d13848c
Andrew <easterbrook@gmail.com>
2018-08-01 16:02:56 +1200

    Update legislation.govt urls

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,11 +4,13 @@ const common = require("./webpack.common.js");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
 
 module.exports = merge(common.webpack(["./src/manifest/production.json"]), {
-	mode: "production",
-	plugins: [
-		new webpack.DefinePlugin({
-			"API_URL": "https://api.openlaw.nz/graphql"
-		}),
-		new CleanWebpackPlugin(common.distBase)
-	]
-});
+    mode: "production",
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                "API_URL": JSON.stringify("https://api.openlaw.nz/graphql")
+            }
+        }),
+        new CleanWebpackPlugin(common.distBase)
+	]}
+);
